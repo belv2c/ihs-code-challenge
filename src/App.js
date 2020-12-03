edf6fb7631eee1935a31c3946f0807346597a547
Thierry Valbrun <1tvalbrun@gmail.com>
2020-12-03 00:48:19 -0500

    I came, I saw, I conquered the challenge! (converted class components to functional)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom";
+import * as React from 'react';
 import './App.css';
 import { Jumbotron, Container } from 'react-bootstrap';
-import Albums from "./components/albums";
-import Counter from "./components/counter";
-import Toggle from "./components/toggle";
+import Albums from './components/albums';
+import Counter from './components/counter';
+import Toggle from './components/toggle';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
       <Jumbotron fluid>
         <Container>
           <h2>Welcome to the IHS Markit Code Challenge!</h2>
         </Container>
       </Jumbotron>
-      <div className="container">
+      <div className='container'>
         <Albums />
         <Counter />
         <Toggle />
       </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/albums.js
+++ src/components/albums.js
@@ -1,50 +1,54 @@
-import React, { Component } from "react";
+import * as React from 'react';
 import { Button, Card, Container, Row, Col } from 'react-bootstrap';
 import '../App.css';
 import PetSoundsCover from '../PetSoundsCover.jpg';
 import RevolverCover from '../RevolverCover.jpg';
 
-class Albums extends Component {
-  state = {
+const Albums = () => {
+  const [classicAlbum, setClassicAlbum] = React.useState({
     image: RevolverCover,
     releaseYear: 1966,
-    name: "The Beatles",
-    album: "Revolver",
-    used: false
-  };
+    name: 'The Beatles',
+    album: 'Revolver',
+    used: false,
+  });
 
-  switchAlbum = () => {
-    this.setState({
+  const switchAlbum = () => {
+    setClassicAlbum({
       image: PetSoundsCover,
       releaseYear: 1966,
-      name: "The Beach Boys",
-      album: "Pet Sounds",
-      used: true
+      name: 'The Beach Boys',
+      album: 'Pet Sounds',
+      used: true,
     });
   };
 
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col>
+  return (
+    <Container>
+      <Row>
+        <Col>
           <h2>Challenge 1: Switch Albums</h2>
-          <Card className="Album" style={{ width: '18rem', display: 'inline-flex', margin: '50px' }}>
-            <Card.Img variant="top" src={this.state.image} />
-              <Card.Body>
-                  <Card.Text>{this.state.album}</Card.Text>
-                  <Card.Text>{this.state.name}</Card.Text>
-                  <Card.Text>{this.state.releaseYear}</Card.Text>
-                  <Card.Text>{this.state.used ? "Used Album" : "New Album"}</Card.Text>
-                  <Button variant="outline-success" onClick={this.switchAlbum}>Switch Album</Button>
-              </Card.Body>
+          <Card
+            className='Album'
+            style={{ width: '18rem', display: 'inline-flex', margin: '50px' }}
+          >
+            <Card.Img variant='top' src={classicAlbum.image} />
+            <Card.Body>
+              <Card.Text>{classicAlbum.album}</Card.Text>
+              <Card.Text>{classicAlbum.name}</Card.Text>
+              <Card.Text>{classicAlbum.releaseYear}</Card.Text>
+              <Card.Text>
+                {classicAlbum.used ? 'Used Album' : 'New Album'}
+              </Card.Text>
+              <Button variant='outline-success' onClick={switchAlbum}>
+                Switch Album
+              </Button>
+            </Card.Body>
           </Card>
-          </Col>
-        </Row>
-      </Container>
-       
-    );
-  }
-}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Albums;
