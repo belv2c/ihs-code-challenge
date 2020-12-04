50559b3d0b02888e4f4fc2164c468fdf22ee095a
samya shajrawi <samiashajrawi@gmail.com>
2020-12-04 00:44:43 -0700

    challenge fixed

--- src/components/albums.js
+++ src/components/albums.js
@@ -1,20 +1,21 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Button, Card, Container, Row, Col } from 'react-bootstrap';
 import '../App.css';
 import PetSoundsCover from '../PetSoundsCover.jpg';
 import RevolverCover from '../RevolverCover.jpg';
 
-class Albums extends Component {
-  state = {
+const Albums = () => {
+  const [state, setState] = useState({
     image: RevolverCover,
     releaseYear: 1966,
     name: "The Beatles",
     album: "Revolver",
     used: false
-  };
+  });
+  
 
-  switchAlbum = () => {
-    this.setState({
+  const switchAlbum = () => {
+    setState({
       image: PetSoundsCover,
       releaseYear: 1966,
       name: "The Beach Boys",
@@ -23,20 +24,20 @@ class Albums extends Component {
     });
   };
 
-  render() {
+
     return (
       <Container>
         <Row>
           <Col>
           <h2>Challenge 1: Switch Albums</h2>
           <Card className="Album" style={{ width: '18rem', display: 'inline-flex', margin: '50px' }}>
-            <Card.Img variant="top" src={this.state.image} />
+            <Card.Img variant="top" src={state.image} />
               <Card.Body>
-                  <Card.Text>{this.state.album}</Card.Text>
-                  <Card.Text>{this.state.name}</Card.Text>
-                  <Card.Text>{this.state.releaseYear}</Card.Text>
-                  <Card.Text>{this.state.used ? "Used Album" : "New Album"}</Card.Text>
-                  <Button variant="outline-success" onClick={this.switchAlbum}>Switch Album</Button>
+                  <Card.Text>{state.album}</Card.Text>
+                  <Card.Text>{state.name}</Card.Text>
+                  <Card.Text>{state.releaseYear}</Card.Text>
+                  <Card.Text>{state.used ? "Used Album" : "New Album"}</Card.Text>
+                  <Button variant="outline-success" onClick={switchAlbum}>Switch Album</Button>
               </Card.Body>
           </Card>
           </Col>
@@ -44,7 +45,6 @@ class Albums extends Component {
       </Container>
        
     );
-  }
 }
 
 export default Albums;
