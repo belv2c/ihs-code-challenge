85fda4ed5728583046afeae6489b29c43bb35e00
Val Magheru <vmm6625@gmail.com>
2020-12-10 19:05:35 -0500

    refactoring components

--- src/components/albums.js
+++ src/components/albums.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Button, Card, Container, Row, Col } from 'react-bootstrap';
 import '../App.css';
 import PetSoundsCover from '../PetSoundsCover.jpg';
 import RevolverCover from '../RevolverCover.jpg';
 
-class Albums extends Component {
-  state = {
+const Albums = () => {
+  const [album, setAlbum] = useState({
     image: RevolverCover,
     releaseYear: 1966,
     name: "The Beatles",
     album: "Revolver",
     used: false
-  };
+  });
 
-  switchAlbum = () => {
-    this.setState({
+  const switchAlbum = () => {
+    setAlbum({
       image: PetSoundsCover,
       releaseYear: 1966,
       name: "The Beach Boys",
@@ -23,28 +23,26 @@ class Albums extends Component {
     });
   };
 
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col>
-          <h2>Challenge 1: Switch Albums</h2>
-          <Card className="Album" style={{ width: '18rem', display: 'inline-flex', margin: '50px' }}>
-            <Card.Img variant="top" src={this.state.image} />
-              <Card.Body>
-                  <Card.Text>{this.state.album}</Card.Text>
-                  <Card.Text>{this.state.name}</Card.Text>
-                  <Card.Text>{this.state.releaseYear}</Card.Text>
-                  <Card.Text>{this.state.used ? "Used Album" : "New Album"}</Card.Text>
-                  <Button variant="outline-success" onClick={this.switchAlbum}>Switch Album</Button>
-              </Card.Body>
-          </Card>
-          </Col>
-        </Row>
-      </Container>
-       
-    );
-  }
-}
+  return (
+    <Container>
+      <Row>
+        <Col>
+        <h2>Challenge 1: Switch Albums</h2>
+        <Card className="Album" style={{ width: '18rem', display: 'inline-flex', margin: '50px' }}>
+          <Card.Img variant="top" src={album.image} />
+            <Card.Body>
+                <Card.Text>{album.album}</Card.Text>
+                <Card.Text>{album.name}</Card.Text>
+                <Card.Text>{album.releaseYear}</Card.Text>
+                <Card.Text>{album.used ? "Used Album" : "New Album"}</Card.Text>
+                <Button variant="outline-success" onClick={switchAlbum}>Switch Album</Button>
+            </Card.Body>
+        </Card>
+        </Col>
+      </Row>
+    </Container>
+      
+  );
+};
 
 export default Albums;
