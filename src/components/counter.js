a4fca25fbf137002263b82879176b142f3f57faf
Tony Fletcher <tfletch55@gmail.com>
2021-01-11 16:25:24 -0500

    Converted components to stateful functional components

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,27 +1,17 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 
-class Counter extends Component {
-  state = {
-    count: 0
-  };
-  add = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-  subtract = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
-
-  render() {
-    return (
-      <div style={{ marginBottom: "50px" }}>
-        <h2>Challenge 2: Counter</h2>
-        <p>Count is: {this.state.count}</p>
-        <Button variant="outline-success" onClick={this.add}>+</Button>{' '}
-        <Button variant="outline-danger" onClick={this.subtract}>-</Button>{' '}
-      </div>
-    );
-  }
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  
+  return (
+    <div style={{ marginBottom: "50px" }}>
+      <h2>Challenge 2: Counter</h2>
+      <p>Count is: {count}</p>
+      <Button variant="outline-success" onClick={() => setCount(count + 1)}>+</Button>{' '}
+      <Button variant="outline-danger" onClick={() => setCount(count - 1)}>-</Button>{' '}
+    </div>
+  );
 }
 
 export default Counter;
