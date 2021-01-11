122ebd2b42c1f6447ec782c5e9755cee992f82cb
Joseph Jalbert <joseph.a.jalbert@gmail.com>
2021-01-11 09:49:17 -0500

    refactor toggle

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -1,24 +1,22 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Surprise from "./surprise";
 import Button from 'react-bootstrap/Button';
 
-class Toggle extends Component {
-  state = {
-    showSurprise: true
-  };
-  revealSurprise = () => {
-    this.setState({ showSurprise: !this.state.showSurprise });
+const Toggle = () => {
+
+  const [showSurprise, setSurprise] = useState(true);
+
+  const revealSurprise = () => {
+    setSurprise(!showSurprise);
   };
 
-  render() {
-    return (
-      <div style={{ margin: "20px" }}>
-        <h2>Challenge 3: Toggle</h2>
-        <Button variant="outline-success" onClick={this.revealSurprise}>{!this.state.showSurprise ? "Show" : "Remove" } the cute sloth</Button>
-        {this.state.showSurprise ? <Surprise /> : ""}
-      </div>
-    );
-  }
-}
+  return (
+    <div style={{ margin: "20px" }}>
+      <h2>Challenge 3: Toggle</h2>
+      <Button variant="outline-success" onClick={revealSurprise}>{!showSurprise ? "Show" : "Remove" } the cute sloth</Button>
+      {showSurprise ? <Surprise /> : ""}
+    </div>
+  );
+};
 
 export default Toggle;
