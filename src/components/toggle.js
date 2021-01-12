b98ef9297ffb49b3f800e3c838d9e7388b6b4896
brandonshirah89 <somwheruphigh89@gmail.com>
2021-01-12 12:29:11 -0500

    Updated class components to stateful functional components

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -1,24 +1,24 @@
-import React, { Component } from "react";
+
+import React, { useState } from "react";
 import Surprise from "./surprise";
 import Button from 'react-bootstrap/Button';
 
-class Toggle extends Component {
-  state = {
-    showSurprise: true
-  };
-  revealSurprise = () => {
-    this.setState({ showSurprise: !this.state.showSurprise });
-  };
+const Toggle = () => {
 
-  render() {
+    const [state, setState ] = useState({showSurprise: true})
+    const {showSurprise} = state
+
+    const revealSurprise = () => {
+        setState({showSurprise: !showSurprise });
+    }
+
     return (
-      <div style={{ margin: "20px" }}>
-        <h2>Challenge 3: Toggle</h2>
-        <Button variant="outline-success" onClick={this.revealSurprise}>{!this.state.showSurprise ? "Show" : "Remove" } the cute sloth</Button>
-        {this.state.showSurprise ? <Surprise /> : ""}
-      </div>
-    );
-  }
+        <div style={{ margin: "20px" }}>
+          <h2>Challenge 3: Toggle</h2>
+          <Button variant="outline-success" onClick={revealSurprise}>{!showSurprise ? "Show" : "Remove" } the cute sloth</Button>
+          {showSurprise ? <Surprise /> : ""}
+        </div>
+      );
 }
 
 export default Toggle;
