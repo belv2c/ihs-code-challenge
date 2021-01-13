d679786b9662adade542cab16031e5c0c4ade35c
ren trigoso <rtrigoso@cbcnewmedia.com>
2021-01-13 15:41:18 -0500

    switches component to use hooks instead of react class layouts as requested on test

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,27 +1,26 @@
-import React, { Component } from "react";
+import { useState, useCallback } from 'react';
 import Button from 'react-bootstrap/Button';
 
-class Counter extends Component {
-  state = {
-    count: 0
+export const useCountFactory = () => {
+  const [count, setCount] = useState(0);
+  const add = useCallback(() => setCount(count => count + 1), []);
+  const subtract = useCallback(() => setCount(count => count - 1), [])
+  return {
+    count,
+    add,
+    subtract
   };
-  add = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-  subtract = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
+};
 
-  render() {
-    return (
-      <div style={{ marginBottom: "50px" }}>
-        <h2>Challenge 2: Counter</h2>
-        <p>Count is: {this.state.count}</p>
-        <Button variant="outline-success" onClick={this.add}>+</Button>{' '}
-        <Button variant="outline-danger" onClick={this.subtract}>-</Button>{' '}
-      </div>
-    );
-  }
-}
+export default function Counter() {
+  const { count, add, subtract } = useCountFactory();
 
-export default Counter;
+  return (
+    <div style={{ marginBottom: "50px" }}>
+      <h2>Challenge 2: Counter</h2>
+      <p>Count is: {count}</p>
+      <Button variant="outline-success" onClick={add}>+</Button>{' '}
+      <Button variant="outline-danger" onClick={subtract}>-</Button>{' '}
+    </div>
+  );
+};
