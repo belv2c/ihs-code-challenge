480880034d1530726c1a2abb718fedcae422db39
Gibran <gibran9@gmail.com>
2021-01-13 14:43:31 -0500

    Converted albums.js, counter.js, surprise.js, and toogle.js into functional components

--- src/components/albums.js
+++ src/components/albums.js
@@ -1,50 +1,65 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Button, Card, Container, Row, Col } from 'react-bootstrap';
 import '../App.css';
 import PetSoundsCover from '../PetSoundsCover.jpg';
 import RevolverCover from '../RevolverCover.jpg';
 
-class Albums extends Component {
-  state = {
-    image: RevolverCover,
-    releaseYear: 1966,
-    name: "The Beatles",
-    album: "Revolver",
-    used: false
-  };
+const initialState = {
+  image: RevolverCover,
+  releaseYear: 1966,
+  name: "The Beatles",
+  album: "Revolver",
+  used: false
+}
+const beachBoys = {
+  image: PetSoundsCover,
+  releaseYear: 1966,
+  name: "The Beach Boys",
+  album: "Pet Sounds",
+  used: true
+}
 
-  switchAlbum = () => {
-    this.setState({
-      image: PetSoundsCover,
-      releaseYear: 1966,
-      name: "The Beach Boys",
-      album: "Pet Sounds",
-      used: true
-    });
-  };
+const Albums = () => {
+  const [state, setState] = useState(initialState)
 
-  render() {
-    return (
-      <Container>
-        <Row>
-          <Col>
+  const switchAlbum = () => {
+    if (state.album === "Revolver") {
+      setState((state) => {
+        return {
+          ...state,
+          ...beachBoys
+        }
+      });
+    } else {
+      setState((state) => {
+        return {
+          ...state,
+          ...initialState
+        }
+      });
+    }
+  };
+  const { image, releaseYear, name, album, used } = state;
+  return (
+    <Container>
+      <Row>
+        <Col>
           <h2>Challenge 1: Switch Albums</h2>
           <Card className="Album" style={{ width: '18rem', display: 'inline-flex', margin: '50px' }}>
-            <Card.Img variant="top" src={this.state.image} />
-              <Card.Body>
-                  <Card.Text>{this.state.album}</Card.Text>
-                  <Card.Text>{this.state.name}</Card.Text>
-                  <Card.Text>{this.state.releaseYear}</Card.Text>
-                  <Card.Text>{this.state.used ? "Used Album" : "New Album"}</Card.Text>
-                  <Button variant="outline-success" onClick={this.switchAlbum}>Switch Album</Button>
-              </Card.Body>
+            <Card.Img variant="top" src={image} />
+            <Card.Body>
+              <Card.Text>{album}</Card.Text>
+              <Card.Text>{name}</Card.Text>
+              <Card.Text>{releaseYear}</Card.Text>
+              <Card.Text>{used ? "Used Album" : "New Album"}</Card.Text>
+              <Button variant="outline-success" onClick={switchAlbum}>Switch Album</Button>
+            </Card.Body>
           </Card>
-          </Col>
-        </Row>
-      </Container>
-       
-    );
-  }
+        </Col>
+      </Row>
+    </Container>
+
+  );
 }
 
 export default Albums;
