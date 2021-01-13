935b900ae0f99e230e1cab059fa2713a99cfb955
mattalco <alcorn@fastmail.com>
2021-01-13 09:31:11 -0500

    utilize functional components and react hooks

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,27 +1,27 @@
-import React, { Component } from "react";
-import Button from 'react-bootstrap/Button';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
 
-class Counter extends Component {
-  state = {
-    count: 0
-  };
-  add = () => {
-    this.setState({ count: this.state.count + 1 });
-  };
-  subtract = () => {
-    this.setState({ count: this.state.count - 1 });
-  };
+function Counter() {
+  const [count, setCount] = useState(0),
+    add = () => {
+      setCount(count + 1);
+    },
+    subtract = () => {
+      setCount(count - 1);
+    };
 
-  render() {
-    return (
-      <div style={{ marginBottom: "50px" }}>
-        <h2>Challenge 2: Counter</h2>
-        <p>Count is: {this.state.count}</p>
-        <Button variant="outline-success" onClick={this.add}>+</Button>{' '}
-        <Button variant="outline-danger" onClick={this.subtract}>-</Button>{' '}
-      </div>
-    );
-  }
+  return (
+    <div style={{ marginBottom: "50px" }}>
+      <h2>Challenge 2: Counter</h2>
+      <p>Count is: {count}</p>
+      <Button variant="outline-success" onClick={add}>
+        +
+      </Button>{" "}
+      <Button variant="outline-danger" onClick={subtract}>
+        -
+      </Button>{" "}
+    </div>
+  );
 }
 
 export default Counter;
