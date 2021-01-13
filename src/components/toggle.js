be5f6dec341d4e6bce978127f9d536486d37ab8e
ren trigoso <rtrigoso@cbcnewmedia.com>
2021-01-13 16:24:16 -0500

    swaps toggle implementation to use hooks instead of react component class as requested by test

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -1,24 +1,24 @@
-import React, { Component } from "react";
+import { useState, useCallback } from 'react';
 import Surprise from "./surprise";
 import Button from 'react-bootstrap/Button';
 
-class Toggle extends Component {
-  state = {
-    showSurprise: true
+export const useSurpriseFactory = () => {
+  const [show, showSurprise] = useState(true);
+  const toggle = useCallback(() => showSurprise(show => !show), []);
+  return {
+    show,
+    toggle
   };
-  revealSurprise = () => {
-    this.setState({ showSurprise: !this.state.showSurprise });
-  };
+};
 
-  render() {
-    return (
-      <div style={{ margin: "20px" }}>
-        <h2>Challenge 3: Toggle</h2>
-        <Button variant="outline-success" onClick={this.revealSurprise}>{!this.state.showSurprise ? "Show" : "Remove" } the cute sloth</Button>
-        {this.state.showSurprise ? <Surprise /> : ""}
-      </div>
-    );
-  }
-}
+export default function Toggle() {
+  const { show, toggle } = useSurpriseFactory();
 
-export default Toggle;
+  return (
+    <div style={{ margin: "20px" }}>
+      <h2>Challenge 3: Toggle</h2>
+      <Button variant="outline-success" onClick={toggle}>{!show ? "Show" : "Remove"} the cute sloth</Button>
+      {show ? <Surprise /> : ""}
+    </div>
+  );
+};
