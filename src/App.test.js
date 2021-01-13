03d175599e1a95f505d5b4d26d00c043c3f6538b
ren trigoso <rtrigoso@cbcnewmedia.com>
2021-01-12 22:56:18 -0500

    added some quick tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
+import Albums from './components/albums';
+
+const acceptableRenderTime = 1000;
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Welcome to the IHS Markit Code Challenge!/i); // TODO: This initial test is not passing. I think it was left over from the boilerplate. Switching the text to something that can be found on the working app.
   expect(linkElement).toBeInTheDocument();
 });
+
+describe('render albums', () => {
+  render(<Albums />);
+  const albums = document.querySelector('div.container');
+  const button = document.querySelector('div.container button');
+
+  test('default album is Revolver by The Beatles', () => {
+    expect(albums).toHaveTextContent("The Beatles");
+    expect(albums).toHaveTextContent("Revolver");
+  });
+
+  test('after a button click, the album is Pet Sounds by The Beach Boys', async () => {
+    userEvent.click(button);
+    setTimeout(() => {
+      expect(albums).toHaveTextContent("The Beach Boys");
+      expect(albums).toHaveTextContent("Pet Sounds");
+    }, acceptableRenderTime)
+  })
+})
